f5498341adc8f8c385aa3e63198a42638d7a79a3
DenisYakovlev <denis.yakovlev098@gmail.com>
2026-06-25 22:51:33 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/glass-images/' : '/',
   plugins: [react(), tailwindcss()],
 })
